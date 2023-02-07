be77212eafe72c02be58a6e4348fc6985acbef8a
frizkall <104696245+frizkall@users.noreply.github.com>
2023-02-06 19:04:07 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/guessnumber/data/GuessNumberDao.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.guessnumber.data;
+
+import com.mycompany.guessnumber.models.Game;
+import com.mycompany.guessnumber.models.Trial;
+import java.util.List;
+
+/**
+ *
+ * @author fares
+ */
+public interface GuessNumberDao {
+    
+    // Create a game
+    Game createGame(Game game);
+    
+    // Create a Try
+    Trial createTrial(Trial trial);
+    
+    // Get all games
+    List<Game> getAllGames();
+    
+    // Get all trials
+    List<Trial> getAllTrials();
+    
+    // Get a specific game
+    Game findGameById(int id);
+    
+    // Get a specific trial
+    Trial findTrialById(int id);
+    
+    // Get list of rounds for a game
+    List<Trial> findTrialsByGameId(int id);
+    
+    // Update game
+    boolean updateGame(Game game);
+    
+    // Delete game
+    boolean deleteGameById(int gameId);
+    
+    // Delete trial
+    boolean deleteTrialById(int trialId);
+}
